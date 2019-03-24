1628e3fc3b65a8ca1ec9d4c360e9982ce0d2cb7b
isra9710 <isra.rios.con@gmail.com>
2019-03-23 23:36:56 -0600

    1.3
    Ya valida el inicio de sesión y ya registra personas

--- proyecto/template.py
+++ proyecto/template.py
@@ -1,49 +1,95 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, flash, redirect, url_for, logging
 import pymysql
 app = Flask(__name__)
-con = pymysql.connect("localhost", "root", "", "base")
-cur = con.cursor()
+app.secret_key = "123"
+conexion = pymysql.connect("localhost", "root", "", "base")
+cur = conexion.cursor()
 @app.route('/')
 def hello():
    return render_template('index.html', name='Isra')
 
 
-@app.route('/user/<nombre>')
-def usuario(nombre='Israel'):
-    edad = 21
-    mi_lista = [1, 2, 3, 4]
-    return render_template('registro.html', nom=nombre, edad=edad, lista=mi_lista)
-
-
-@app.route('/cliente')
-def cliente():
-    lista_nombres = ['Prueba1', 'Prueba2', 'Prueba3']
-    return render_template('cliente.html',  lista=lista_nombres)
-
-
-@app.route('/registro')
+@app.route('/registro', methods=['GET', 'POST'])
 def registro():
-    consulta=cur.execute("select nombre_universidad from Universidad")
+    cur.execute("select nombre_universidad from Universidad")
     listat = cur.fetchall()
     lista = list(sum(listat, ()))
-    for e in lista:
-        print(e)
-    return render_template('registro.html', listaU=lista)
+    if lista is None:
+        flash("No hay universidades registradas, pidele al administrador que registre alguna")
+    else:
+        for e in lista:
+            print(e)
+    return render_template('registroLogin.html', listaU=lista)
 
 
-@app.route('/registrado')
+@app.route('/registrandote', methods=['GET', 'POST'])
 def registrado():
-    return render_template('registrado.html')
+    nombreUni = request.form.get("nombreUni")
+    nombre = request.form.get("nombre")
+    contra = request.form.get("contra")
+    nombreAux=None
+    id_universidad=None
+    print(nombreUni)
+    print(nombre)
+    print(contra)
+    consulta=("select nombre_usuario from Usuario where nombre_usuario=%s;")
+    cur.execute(consulta, (nombre))
+    lista=cur.fetchone()
+    if lista!= None:
+        for e in lista:
+            nombreAux=e
+    print(nombreAux)
+    consulta="select id_universidad from Universidad where nombre_universidad = %s;"
+    cur.execute(consulta, (nombreUni))
+    lista=cur.fetchone()
+    if lista!= None:
+        for e in lista:
+            id_universidad=e
+    print(id_universidad)
+    if request.method =="POST":
+        if nombreAux != nombre:
+                consulta = ("insert into Usuario (id_universidad, nombre_usuario, contra_usuario) values(%s, %s, %s);")
+                cur.execute(consulta, (id_universidad, nombre, contra))
+                conexion.commit()
+                print("terminado")
+                return render_template('registrado.html')
+        else:
+            print("entra al else")
+            flash("Ese correo ya esta registrado")
+            return redirect(url_for('registro'))
 
 
-@app.route('/iniciarSesion')
-def iniciarSesion():
-    return render_template('registro.html')
+@app.route('/iniciandoSesion', methods=['GET', 'POST'])
+def sesionIniciada():
 
+        nombre = request.form.get("nombreInicio")
+        contra = request.form.get("contraInicio")
+        nombreAux=None
+        contraAux=None
+        print(nombre)
+        print(contra)
+        consulta = ("select nombre_usuario from Usuario where nombre_usuario=%s;")
+        cur.execute(consulta, (nombre))
+        lista=cur.fetchone()
+        if lista != None:
+            for e in lista:
+                nombreAux=e
 
-@app.route('/sesionIniciada')
-def sesionIniciad():
-    return render_template('sesionIniciada.html')
+        consulta = ("select contra_usuario from Usuario where contra_usuario=%s;")
+        cur.execute(consulta, (contra))
+        lista=cur.fetchone()
+        if lista!= None:
+            for e in lista:
+                contraAux=e
+
+        print(nombreAux)
+        print(contraAux)
+        if nombre == nombreAux and contra == contraAux:
+            return render_template('sesionIniciada.html')
+        else:
+            print("No se inicio sesion")
+            return redirect(url_for('registro'))
+
 
 
 if __name__ == "__main__":
